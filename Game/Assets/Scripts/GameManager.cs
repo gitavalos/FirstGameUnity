0ab9e98e956173dd733f1919d80ff75ebeeb1f37
DevAvalos <201114339@ingenieria.usac.edu.gt>
2018-01-20 14:15:38 -0600

    Writing the Game Manager

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+
+	private int level = 3;
+
+	void Awake (){
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame ();
+	}
+
+	void InitGame(){
+		boardScript.SetupScene (level);
 	}
 	
 	// Update is called once per frame
@@ -14,3 +29,4 @@ public class GameManager : MonoBehaviour {
 		
 	}
 }
+;
