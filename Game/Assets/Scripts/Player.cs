0a4a2841fd8700cdb2f7ac2e9c5a1c07ff7374a1
DevAvalos <201114339@ingenieria.usac.edu.gt>
2018-01-22 12:11:27 -0600

    Adding Mobile Controls

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : MovingObject {
     public AudioClip drinkSound1;
     public AudioClip drinkSound2;
     public AudioClip gameOverSound;
+    public Vector2 touchOrigin = -Vector2.one;
 
 
 	// Use this for initialization
@@ -42,13 +43,40 @@ public class Player : MovingObject {
 		int horizontal = 0;
 		int vertical = 0;
 
-		horizontal = (int)Input.GetAxisRaw ("Horizontal");
+#if UNITY_EDITOR || UNITY_SANDALONE || UNITY_WEBPLAYER
+        horizontal = (int)Input.GetAxisRaw ("Horizontal");
 		vertical = (int)Input.GetAxisRaw ("Vertical");
 
 		if (horizontal != 0)
 			vertical = 0;
+#else
+        if(Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+            if(myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if(myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
 
-		if (horizontal != 0 || vertical != 0)
+#endif
+
+        if (horizontal != 0 || vertical != 0)
 			AttemptMove<Wall> (horizontal, vertical);
 	}	
 
